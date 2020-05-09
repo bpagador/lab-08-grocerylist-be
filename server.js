1ab490b0b31d7a1750950077ada7119625a0e29b
Briseid Pagador <bpagador@gmail.com>
2020-05-08 18:58:30 -0700

    app get by id, and app post to create new item has been added to server js, console.error throwing up yellow squiggly

--- server.js
+++ server.js
@@ -9,10 +9,35 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
+
 app.get('/grocery_list', async(req, res) => {
   const data = await client.query('SELECT * from grocery_list');
 
   res.json(data.rows);
+});
+
+app.get('/detail_page/:id', async(req, res) => {
+  const id = req.params.id;
+  const data = await client.query(
+    'SELECT * from grocery_list WHERE id=$1', [id]);
+
+  res.json(data.rows[0]);
+});
+
+app.post('/add_grocery/', async(req, res) => {
+  try {
+    const data = await client.query(
+      `INSERT INTO grocery_list (name, type, amount, owner_id, is_cheap)
+      VALUES ($1, $2, $3, $4, $5)
+      returning *;`,
+      [req.body.name, req.body.type, req.body.amount, 1, req.bosy.isCheap]
+    );
+
+    res.json(data.rows[0]);
+  } catch(e) {
+    console.error ();
+    res.json (e);
+  }
 });
 
 app.listen(PORT, () => {
